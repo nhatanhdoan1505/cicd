134f1dba3f29c8f63c17427853cb35f0fac0dd3a
Doan Nhat Anh <anhfernaldo@gmail.com>
2023-05-25 22:25:36 +0700

    commit 1

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,5 +2,6 @@ import Login from 'modules/login';
 import { withPreAuthentication } from 'common/hocs';
 
 const IndexUnAuthenticated = withPreAuthentication(Login);
+//hello
 
 export default IndexUnAuthenticated;
